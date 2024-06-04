dc4eb6661c19a79da43f2aa0838733cda78ede64
aegilops <41705651+aegilops@users.noreply.github.com>
2024-06-04 15:58:31 +0100

    Change clone command

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-gh repo clone aegilops/ms-reactor-workshop
+git clone https://github.com/aegilops/ms-reactor-workshop-aegilops.git
 cd ms-reactor-workshop || exit 1
 git remote add mirror https://github.com/ghas-bootcamp-2024-04-06-"$1"/ms-reactor-workshop
 git push mirror '*:*' || ( echo "Push failed, did you set up the mirror repository, and do you have you CLI auth set up? Try using a PAT or SSH key" && exit 1 )
