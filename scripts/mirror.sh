835919d42126cf0e0af63ff5524c0ba49bd3abd9
aegilops <41705651+aegilops@users.noreply.github.com>
2024-06-04 16:11:37 +0100

    Switch to general org

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <GitHub handle>"
+  echo "Usage: $0 <your org>"
   exit 1
 fi
 
 git clone https://github.com/aegilops/ms-reactor-workshop-aegilops.git
 cd ms-reactor-workshop || exit 1
-git remote add mirror https://github.com/ghas-bootcamp-2024-04-06-"$1"/ms-reactor-workshop
+git remote add mirror https://github.com/"$1"/ms-reactor-workshop
 git push mirror '*:*' || ( echo "Push failed, did you set up the mirror repository, and do you have you CLI auth set up? Try using a PAT or SSH key" && exit 1 )
